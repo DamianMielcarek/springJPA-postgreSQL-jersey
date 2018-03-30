b882f23746e60db619e989d249ab1f0d5bdc17cb
Damian Mielcarek <damian.mielcarek@gmail.com>
2018-03-30 11:22:53 +0200

    Spring Boot Jersey Restful Web Service

---
+++ src/main/java/com/jersey/SpringJpaPostgreSqlJerseyApplication.java
@@ -0,0 +1,12 @@
+package com.jersey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaPostgreSqlJerseyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaPostgreSqlJerseyApplication.class, args);
+	}
+}
